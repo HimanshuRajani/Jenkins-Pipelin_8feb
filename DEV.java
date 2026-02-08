a617b57fa9c949761d08159a51784fee75bc1cbd
Himanshu Rajani <157615607+HimanshuRajani@users.noreply.github.com>
2026-02-08 12:04:25 +0530

    Rename HelloWorld class to DEV
    Renamed class from HelloWorld to DEV.

--- DEV.java
+++ DEV.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class DEV {
     public static void main(String[] args) {
         System.out.println("Hello, World!!!\n This is from the development file."); // Prints the message to the console
     }
