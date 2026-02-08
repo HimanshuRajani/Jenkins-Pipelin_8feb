466047d533c9c5db60ccb6a0f7d74cae403896ff
Himanshu Rajani <157615607+HimanshuRajani@users.noreply.github.com>
2026-02-08 11:50:58 +0530

    Update console message in DEV.java

---
+++ DEV.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!!!\n This is from the development file."); // Prints the message to the console
+    }
+}
