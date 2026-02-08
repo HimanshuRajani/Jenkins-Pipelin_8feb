bbdc1abef86ca3d3bc08c6ccd032cc56bf045b88
Himanshu Rajani <157615607+HimanshuRajani@users.noreply.github.com>
2026-02-08 12:04:45 +0530

    Rename HelloWorld class to Test

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World!!\n Your Development code is working fine hence you are getting this response"); // Prints the message to the console
     }
